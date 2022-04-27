3a98ca0122284ea0923cc924a311c92888af3cec
KuvonchbekN <knormakhmatov@students.wiut.uz>
2022-04-27 12:22:52 +0500

    first commit

---
+++ src/main/java/by/kuvonchbekn/outlaysbot/OutlaysBotApplication.java
@@ -0,0 +1,13 @@
+package by.kuvonchbekn.outlaysbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutlaysBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OutlaysBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/kuvonchbekn/outlaysbot/OutlaysBotApplicationTests.java
@@ -0,0 +1,13 @@
+package by.kuvonchbekn.outlaysbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutlaysBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
